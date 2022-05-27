c69ea51bfbf68686ab9fcfcee81c2090202414b6
edugabriel12 <gabriellima4539@yahoo.com>
2022-05-27 15:52:11 -0300

    Criação do Projeto

---
+++ src/main/java/com/eduardogabriel/applicationmongo/ApplicationmongoApplication.java
@@ -0,0 +1,13 @@
+package com.eduardogabriel.applicationmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardogabriel/applicationmongo/ApplicationmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardogabriel.applicationmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
