0afe0395e062513b2b5d4dafa5a003c67253c5be
edugabriel12 <gabriellima4539@yahoo.com>
2022-05-28 16:43:16 -0300

    Obtendo um usuário por ID

--- src/main/java/com/eduardogabriel/applicationmongo/services/UserService.java
+++ src/main/java/com/eduardogabriel/applicationmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.eduardogabriel.applicationmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eduardogabriel.applicationmongo.domain.User;
 import com.eduardogabriel.applicationmongo.repository.UserRepository;
+import com.eduardogabriel.applicationmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -14,8 +16,12 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 	
-	public List<User> findAll() {
-		
+	public List<User> findAll() {	
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Pessoa não encontrada")); 
 	}
 }
