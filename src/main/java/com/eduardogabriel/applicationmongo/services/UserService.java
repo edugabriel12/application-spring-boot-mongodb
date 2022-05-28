2490bc2a8f05bfca2e4a2f4718d614a5f560dc82
edugabriel12 <gabriellima4539@yahoo.com>
2022-05-28 19:29:23 -0300

    Inserção de Usuários

--- src/main/java/com/eduardogabriel/applicationmongo/services/UserService.java
+++ src/main/java/com/eduardogabriel/applicationmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eduardogabriel.applicationmongo.domain.User;
+import com.eduardogabriel.applicationmongo.dto.UserDTO;
 import com.eduardogabriel.applicationmongo.repository.UserRepository;
 import com.eduardogabriel.applicationmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Pessoa não encontrada")); 
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getNome(), objDto.getEmail());
 	}
 }
