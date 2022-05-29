b798b8ec11a13616d8db7ad8715556de584b4fd8
edugabriel12 <gabriellima4539@yahoo.com>
2022-05-29 17:09:09 -0300

    Consulta de post com vários critérios

--- src/main/java/com/eduardogabriel/applicationmongo/resources/PostResource.java
+++ src/main/java/com/eduardogabriel/applicationmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.eduardogabriel.applicationmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,10 +30,24 @@ public class PostResource {
 	}
 	
 	@RequestMapping(value="/titlesearch", method = RequestMethod.GET) 
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam String text) {		
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {		
 		text = URL.decodeParam(text);
 		
 		List<Post> posts = service.findByTitulo(text);
+		return ResponseEntity.ok().body(posts);
+	}
+	
+	@RequestMapping(value="/fullsearch", method = RequestMethod.GET) 
+	public ResponseEntity<List<Post>> fullSearch
+			(@RequestParam(value="text", defaultValue="") String text,
+			 @RequestParam(value="minDate", defaultValue="") String minDate,
+			 @RequestParam(value="maxDate", defaultValue="") String maxDate) {		
+		
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		
+		List<Post> posts = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(posts);
 	}
 
