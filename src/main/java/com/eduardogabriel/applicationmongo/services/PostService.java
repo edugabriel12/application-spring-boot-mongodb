41ad5cb19424cad0f5222f9ea7eeb6797deb9707
edugabriel12 <gabriellima4539@yahoo.com>
2022-05-28 23:22:45 -0300

    Obtendo posts pelo ID

---
+++ src/main/java/com/eduardogabriel/applicationmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.eduardogabriel.applicationmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardogabriel.applicationmongo.domain.Post;
+import com.eduardogabriel.applicationmongo.repository.PostRepository;
+import com.eduardogabriel.applicationmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado")); 
+	}
+
+}
